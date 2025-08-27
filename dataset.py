fb087fe4b0f0317cd7d3bfebdbb3634260f71c21
Saiyara <saiyaraislam17@gmail.com>
2025-08-27 15:47:07 -0400

    seq_extraction

--- dataset.py
+++ dataset.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import os
-
-
 from pandas import read_parquet
 
 path = "/data/benlab/DeepDD/GENEActiv_Data/Data/DEEP DD-GENEActiv Data/"
@@ -12,7 +10,8 @@ def open_csv(csv_path):
     df = pd.read_csv(csv_path, skiprows=100)
     df.to_parquet("./parquet/"+csv_path.strip(".csv")+".parquet")
 
+def get_seq(label, data_path, name):
+    df = pd.read_parquet(data_path)
+    df_new = df[df["date"] == label]
+    df_new.to_parquet(name, index=False)
 
-
-if __name__ == "__main__":
-   open_csv("DD072024007_left wrist_101696_2024-10-22 15-22-36.csv")
---
+++ main.py
@@ -0,0 +1,4 @@
+from dataset import *
+
+get_seq(label="8/20/2024", data_path="cleaned_parquet/007/Month 1", name=".")
+
