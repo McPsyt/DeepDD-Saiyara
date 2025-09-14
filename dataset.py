338de2f3083c92ce4a691090a836cc393c68bc60
Saiyara Islam -- benlab RA <islsai@dnpws21.douglas.rtss.qc.ca>
2025-09-14 15:28:24 -0400

    Organized code after documenting in data PES

--- dataset.py
+++ dataset.py
@@ -9,9 +9,3 @@ path = "/data/benlab/DeepDD/GENEActiv_Data/Data/DEEP DD-GENEActiv Data/"
 def open_csv(csv_path):
     df = pd.read_csv(csv_path, skiprows=100)
     df.to_parquet("./parquet/"+csv_path.strip(".csv")+".parquet")
-
-def get_seq(label, data_path, name):
-    df = pd.read_parquet(data_path)
-    df_new = df[df["date"] == label]
-    df_new.to_parquet(name, index=False)
-
